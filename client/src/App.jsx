c0248b87ba380202b9d044b6b35610f77b5bf739
compgirl123 <claudia.f.feochari@hotmail.com>
2023-10-29 22:38:06 -0400

    code efficiency improved

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,68 +1,67 @@
-import React, {useState, useEffect} from 'react'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import './App.css';
 import Axios from "axios";
 import Card from "./components/card";
 
 function App() {
+  const baseUrl = "https://managerztododb.onrender.com";
 
-    //const baseUrl = "http://localhost:3001"
-    const baseUrl = "https://managerztododb.onrender.com"
+  const [values, setValues] = useState({ name: '', category: '' });
+  const [games, setGames] = useState([]);
 
-    const [values, setValues] = useState({ name: '', category: '' });
-    const [games, setGames] = useState([]);
+  // useEffect to load data initially and when values.name or values.category change
+  useEffect(() => {
+    fetchGames();
+  }, [games]);
 
-     useEffect(() => {
-      Axios.get(`${baseUrl}/games`)
-          .then((response) => {
-              setGames(response.data);
-          });
-    },[games]); // Empty dependency array to run the effect only once
+  const fetchGames = () => {
+    Axios.get(`${baseUrl}/games`, {
+      params: { name: values.name, category: values.category }
+    }).then((response) => {
+      setGames(response.data);
+    });
+  }
 
-    const handleChangeValues = (value) => {
-        setValues((prevValue) => ({
-            ...prevValue,
-            [value.target.name]: value.target.value,
-        }))
-    }
+  const handleChangeValues = (value) => {
+    setValues(prevValue => ({
+      ...prevValue,
+      [value.target.name]: value.target.value,
+    }));
+  }
 
-    const handleClickButton = () => {
-        //setValues({ name: '', category: '' });
-        setValues({ name: '', category: '' }); // Clear the input fields
-        Axios.post(`${baseUrl}/register`, {
-            name: values.name,
-            category: values.category,
-        }).then((response) =>{
-            console.log(response)
-            setGames([...games,response.data]);
-        });
-    }
-
+  const handleClickButton = () => {
+    setValues({ name: '', category: '' });
+    Axios.post(`${baseUrl}/register`, {
+      name: values.name,
+      category: values.category,
+    }).then((response) => {
+      setGames([...games, response.data]); // Update the local state with the new entry
+    });
+  }
 
   return (
     <div className="App">
       <div className="container">
-          <h1 className="title">Manager's To Do's</h1>
-          <div className="register-box">
-              <input className="register-input" type="text" name="name" placeholder="Title"  value={values.name} onChange={handleChangeValues} />
-              <input className="register-input" type="text" name="category" placeholder="Category" value={values.category} onChange={handleChangeValues} />
-              <button className="register-button" onClick={handleClickButton}>Add</button>
-          </div>
-          <br/>
-          <div className="cards">
-              {typeof games !== 'undefined' &&
-                  games.map((game) => {
-                      return <Card
-                          key={game.id}
-                          id={game.id}
-                          name={game.name}
-                          category={game.category}
-                          >
-                      </Card>;
-                  })}
-          </div>
+        <h1 className="title">Manager's To Do's</h1>
+        <div className="register-box">
+          <input className="register-input" type="text" name="name" placeholder="Title" value={values.name} onChange={handleChangeValues} />
+          <input className="register-input" type="text" name="category" placeholder="Category" value={values.category} onChange={handleChangeValues} />
+          <button className="register-button" onClick={handleClickButton}>Add</button>
+        </div>
+        <br />
+        <div className="cards">
+          {games.map((game) => (
+            <Card
+              key={game.id}
+              id={game.id}
+              name={game.name}
+              category={game.category}
+            />
+          ))}
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- server/index.js
+++ server/index.js
@@ -15,12 +15,20 @@ const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
-  //connectionLimit: 10
+  database: process.env.DB_DATABASE,
+  connectionLimit: 10
 })
 
 server.use(express.json());
 server.use(cors());
+
+let queryCount = 0; // Initialize the query count
+
+// Middleware to increment the query count
+server.use((req, res, next) => {
+  queryCount += 1; // Increment the query count for every request
+  next();
+});
 
 server.post("/register", (req, res) => {
     const { name } = req.body;
@@ -72,3 +80,17 @@ server.delete("/delete/:index", (req,res) =>{
 server.listen(3001, () =>
     console.log("Running in the port 3001")
 );
+
+server.get('/running-queries', (req, res) => {
+  db.query('SHOW PROCESSLIST', (err, results) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'Unable to fetch running queries' });
+    }
+    res.json(results);
+  });
+});
+
+server.get('/query-count', (req, res) => {
+  res.json({ queryCount });
+});
