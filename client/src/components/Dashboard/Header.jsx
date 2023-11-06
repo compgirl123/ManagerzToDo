61fa70cac02e27659581e5f97749a1c06029c1c1
compgirl123 <claudia.f.feochari@hotmail.com>
2023-11-05 21:00:38 -0500

    Made ui better

--- client/src/components/Dashboard/Header.jsx
+++ client/src/components/Dashboard/Header.jsx
@@ -6,7 +6,7 @@ const Header = ({ setIsAdding, setIsAuthenticated }) => {
   return (
     <header>
       <h1 className="managerHeader">Manager's To Do's</h1>
-      <div style={{ marginTop: '30px', marginBottom: '18px' }}>
+      <div style={{ marginTop: '30px', marginBottom: '18px' }} className="logout">
         <Logout setIsAuthenticated={setIsAuthenticated} />
       </div>
     </header>
